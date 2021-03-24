3f15f450b81c73c43dee107213fc5ef92a382555
William Fondrie <fondriew@gmail.com>
2021-03-24 16:57:23 -0700

    Upgraded boost and darwin build

---
+++ scripts/misc/update_boost.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+set -e
+cd "$(dirname "$0")"
+
+# The Boost version to download:
+boost_version=1.75.0
+
+# Create a temporary directory to work in:
+tmp_dir=$(mktemp -d -t ci-XXXXXXXXXX)
+cd ${tmp_dir}
+
+# Download and unpack the boost tarball:
+base=boost_$(tr "." "_" <<< ${boost_version})
+
+echo "Downloading Boost tarball..." > /dev/stderr
+curl -0L https://dl.bintray.com/boostorg/release/${boost_version}/source/${base}.tar.bz2 \
+     -o ${base}.tar.bz2
+
+echo "Unpacking Boost tarball..." > /dev/stderr
+tar -xjf ${base}.tar.bz2
+
+# Remove the directories suggested by Matt, then repack:
+# Note that the libs/test directory is needed to avoid a build error.
+echo "Trimming Boost..." > /dev/stderr
+rm -r ${base}/doc
+echo "Re-packing Boost into tarball..." > /dev/stderr
+tar -cjSf ${base}.tar.bz2 ${base}
+
+# Add to the pwiz repo:
+cd - > /dev/null
+rm ../../libraries/boost_*.tar.bz2
+cp ${tmp_dir}/${base}.tar.bz2 ../../libraries
+
+# Update Jamroot.jam:
+echo "Updating Jamroot.jam..." > /dev/stderr
+echo "  -> Changes:" > /dev/stderr
+sed -i "" -re "s/boost(_[0-9]+)+/${base}/gw /dev/stderr" ../../Jamroot.jam
+
+echo "DONE!" > /dev/stderr
